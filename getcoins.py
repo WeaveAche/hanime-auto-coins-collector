fc66313f8450b1a1a64f85cb1dc376de392faaf0
Praveensenpai <71966071+Praveensenpai@users.noreply.github.com>
2023-02-02 12:45:17 +0530

    coins_generator.py
    removed json module becz requests already have json function in a response
    added fstrings
    and minor improvements

--- getcoins.py
+++ getcoins.py
@@ -1,22 +1,19 @@
-import json
+from contextlib import suppress
 import os
 import time
 from hashlib import sha256
-
 import requests
 from dateutil import parser
 from dotenv import load_dotenv
 
 load_dotenv()
 
-host = "https://www.universal-cdn.com"
+HOST = "https://www.universal-cdn.com"
+HANIME_EMAIL = os.getenv("hanime_email")
+HANIME_PASSWORD = os.getenv("hanime_password")
 
-hanime_email = os.getenv('hanime_email')
-hanime_password = os.getenv('hanime_password')
 
-
-def getSHA256(to_hash):
-    """Get SHA256 hash."""
+def getSHA256(to_hash) -> str:
     m = sha256()
     m.update(to_hash.encode())
     return m.hexdigest()
@@ -29,89 +26,106 @@ def getXHeaders():
     """
     XClaim = str(int(time.time()))
     XSig = getSHA256(f"9944822{XClaim}8{XClaim}113")
-    headers = {"X-Signature-Version": "app2", "X-Claim": XClaim, "X-Signature": XSig}
-    return headers
+    return {"X-Signature-Version": "app2", "X-Claim": XClaim, "X-Signature": XSig}
 
 
-def login(s: requests.Session, email, password):
-    """Login into your hanime account."""
-    s.headers.update(getXHeaders())
-    response = s.post(f"{host}/rapi/v4/sessions",
-                      headers={"Content-Type": "application/json;charset=utf-8"},
-                      data=f'{{"burger":"{email}","fries":"{password}"}}')
-
-    if '{"errors":["Unauthorized"]}' in response.text:
-        raise SystemExit("[!!!] Login failed, please check your credentials.")
-
-    return getInfo(response.text)
-
-
-def getInfo(response):
+def get_info(response_json: list[dict] | dict):
     """Parse out only relevant info."""
-    received = json.loads(response)
 
     ret = {
-        "session_token": received["session_token"],
-        "uid": received["user"]["id"],
-        "name": received["user"]["name"],
-        "coins": received["user"]["coins"],
-        "last_clicked": received["user"]["last_rewarded_ad_clicked_at"]
+        "session_token": response_json["session_token"],
+        "uid": response_json["user"]["id"],
+        "name": response_json["user"]["name"],
+        "coins": response_json["user"]["coins"],
+        "last_clicked": response_json["user"]["last_rewarded_ad_clicked_at"],
     }
 
-    available_keys = list(received["env"]["mobile_apps"].keys())
+    available_keys = list(response_json["env"]["mobile_apps"].keys())
 
     if "_build_number" in available_keys:
-        ret["version"] = received["env"]["mobile_apps"]["_build_number"]
+        ret["version"] = response_json["env"]["mobile_apps"]["_build_number"]
     elif "osts_build_number" in available_keys:
-        ret["version"] = received["env"]["mobile_apps"]["osts_build_number"]
+        ret["version"] = response_json["env"]["mobile_apps"]["osts_build_number"]
     elif "severilous_build_number" in available_keys:
-        ret["version"] = received["env"]["mobile_apps"]["severilous_build_number"]
+        ret["version"] = response_json["env"]["mobile_apps"]["severilous_build_number"]
     else:
-        raise SystemExit("[!!!] Unable to find the build number for the latest mobile app, please report an issue on github.")
+        print(
+            "[!!!] Unable to find the build number for the latest mobile app, please report an issue on github."
+        )
 
     return ret
 
 
-def getCoins(s: requests.Session, version, uid):
+def login(session: requests.Session, email: str, password: str) -> dict:
+    """Login into your hanime account."""
+    session.headers.update(getXHeaders())
+    response = session.post(
+        f"{HOST}/rapi/v4/sessions",
+        headers={"Content-Type": "application/json;charset=utf-8"},
+        data=f'{{"burger":"{email}","fries":"{password}"}}',
+    )
+    if '{"errors":["Unauthorized"]}' in response.text:
+        print("[!!!] Login failed, please check your credentials.")
+
+    return get_info(response.json())
+
+
+def get_coins(session: requests.Session, version: str, uid: str) -> None:
     """
     Send a request to claim your coins, this request is forged and we are not actually clicking the ad.
     Again, reverse engineering the mechanism of generating the reward token wasn't much obfuscated.
     """
-    s.headers.update(getXHeaders())
+    session.headers.update(getXHeaders())
 
-    curr_time = str(int(time.time()))
-    to_hash = f"coins{version}|{uid}|{curr_time}|coins{version}"
+    current_time = str(int(time.time()))
+    to_hash = f"coins{version}|{uid}|{current_time}|coins{version}"
 
-    data = {"reward_token": getSHA256(to_hash) + f"|{curr_time}", "version": f"{version}"}
+    data = {
+        "reward_token": f"{getSHA256(to_hash)}|{current_time}",
+        "version": f"{version}",
+    }
 
-    response = s.post(f"{host}/rapi/v4/coins", data=data)
+    response = session.post(f"{HOST}/rapi/v4/coins", data=data)
 
-    if '{"errors":["Unauthorized"]}' in response.text:
-        raise SystemExit("[!!!] Something went wrong, please report issue on github")
-    print(f"You received {json.loads(response.text)['rewarded_amount']} coins.")
+    if '{"errors":["Unauthorized"]}' not in response.text:
+        print(f"You received {response.json()['rewarded_amount']} coins.")
 
 
 def main():
-    s = requests.Session()
+    session = requests.Session()
 
-    info = login(s, hanime_email, hanime_password)
+    info = login(session, HANIME_EMAIL, HANIME_PASSWORD)
 
-    s.headers.update({"X-Session-Token": info["session_token"]})
+    session.headers.update({"X-Session-Token": info["session_token"]})
 
     print(f"[*] Logged in as {info['name']}")
     print(f"[*] Coins count: {info['coins']}")
 
-    if info['last_clicked'] is not None:
+    if info["last_clicked"] is not None:
         print(f"[*] Last clicked on {parser.parse(info['last_clicked']).ctime()} UTC")
 
         previous_time = parser.parse(info["last_clicked"]).timestamp()
         if time.time() - previous_time < 3 * 3600:
-            raise SystemExit("[!!!] You've already clicked on an ad less than 3 hrs ago.")
+            print("[!!!] You've already clicked on an ad less than 3 hrs ago.")
     else:
-        print(f"[*] Never clicked on an ad")
+        print("[*] Never clicked on an ad")
+        get_coins(session, info["version"], info["uid"])
 
-    getCoins(s, info["version"], info["uid"])
 
+class Generator:
+    def __init__(
+        self, interval: float = 1 * 60 * 60, exception_handling: bool = True
+    ) -> None:
+        self.interval = interval
+        self.exception_handling = exception_handling
 
-if __name__ == "__main__":
-    main()
+    def run(self) -> None:
+        while True:
+            if not self.exception_handling:
+                main()
+            else:
+                with suppress(Exception):
+                    main()
+
+            print(f"sleeping for {self.interval} seconds")
+            time.sleep(self.interval)
